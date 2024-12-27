fd2ea9769f51261ab5a268ef279bfc17802e4384
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-12-27 11:50:02 +0545

    Made title field unique for particular user and in the same date

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 
 const TransactionSchema = new mongoose.Schema({
-    title: { type: String, required: true, unique:true },
+    title: { type: String, required: true },
     amount: { type: String, required: true },
     type: { type: String, required: true },
     date:{ type: Date, required: true },
     userId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true}
 },{timestamps:true});
 
+TransactionSchema.index({title:1, userId:1,date:1},{ unique:true});
 const Transaction = mongoose.model('Transaction', TransactionSchema);
  Transaction.syncIndexes();
 export default Transaction;
